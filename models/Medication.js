c70317a41b8e3227f343b2ba4e6b0628a811a03d
Сияна <you@example.com>
2026-03-26 23:34:12 +0200

    Medivation

--- models/Medication.js
+++ models/Medication.js
@@ -1,13 +1,55 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
-
+/**
+ * Модел Medication
+ * Съхранява информация за лекарствата, графика на прием и контактите за известяване.
+ */
 const Medication = sequelize.define('Medication', {
-    name: { type: DataTypes.STRING, allowNull: false },
-    dosage: { type: DataTypes.STRING },
-    time: { type: DataTypes.TIME, allowNull: false },
-    isTaken: { type: DataTypes.BOOLEAN, defaultValue: false },
-    userId: { type: DataTypes.INTEGER, allowNull: false }
+    // Имейл на потребителя, на който ще се праща първото напомняне
+    userEmail: { 
+        type: DataTypes.STRING, 
+        allowNull: false,
+        validate: {
+            isEmail: true
+        }
+    },
+    // Имейл на близък човек, ако потребителят не отбележи, че е взел хапчето
+    emergencyEmail: { 
+        type: DataTypes.STRING, 
+        allowNull: false,
+        validate: {
+            isEmail: true
+        }
+    },
+    // Име на лекарството (може да идва от Autocomplete списъка)
+    name: { 
+        type: DataTypes.STRING, 
+        allowNull: false 
+    },
+    // Количество (напр. "1 таблетка", "5мл")
+    dosage: { 
+        type: DataTypes.STRING,
+        allowNull: true 
+    },
+    // Час на прием (напр. "08:00")
+    time: { 
+        type: DataTypes.TIME, 
+        allowNull: false 
+    },
+    // Статус дали е взето - ако остане false след определен час, се праща спешен имейл
+    isTaken: { 
+        type: DataTypes.BOOLEAN, 
+        defaultValue: false 
+    },
+    // Помага на логиката за имейли да знае кога последно е пратено известие
+    lastNotified: {
+        type: DataTypes.DATE,
+        allowNull: true
+    }
+}, {
+    // Автоматично добавя createdAt и updatedAt
+    timestamps: true 
 });
 
-module.exports = Medication; // ВАЖНО: Провери дали този ред го има
+module.exports = Medication;
